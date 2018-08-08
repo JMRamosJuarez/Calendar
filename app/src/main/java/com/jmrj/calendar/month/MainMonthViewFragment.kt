6b25a416902d96883db2f3c6a0ee46a742051e5b
Jesús Manuel Ramos Juárez <jramos@ia.com.mx>
2018-08-08 09:31:00 -0500

    MainMonthViewFragment, show current month

--- app/src/main/java/com/jmrj/calendar/month/MainMonthViewFragment.kt
+++ app/src/main/java/com/jmrj/calendar/month/MainMonthViewFragment.kt
@@ -7,8 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.jmrj.calendar.R
 import kotlinx.android.synthetic.main.main_month_view_fragment_layout.*
+import java.util.*
 
 class MainMonthViewFragment : Fragment() {
+
+    private val calendar: Calendar by lazy { Calendar.getInstance(Locale.getDefault()) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.main_month_view_fragment_layout, container, false)
@@ -17,5 +20,6 @@ class MainMonthViewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         this.months_view_pager.adapter = MonthsFragmentPagerAdapter(this.childFragmentManager)
+        this.months_view_pager.currentItem = this.calendar.get(Calendar.MONTH)
     }
 }
