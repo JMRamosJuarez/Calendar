ecdbf9619e81d698d3862380f96927a4dbe5622a
Jesús Manuel Ramos Juárez <jesusmanuelramosjuarez@gmail.com>
2018-08-07 22:52:27 -0500

    Current hour in week view, initial code

--- app/src/main/java/com/jmrj/calendar/week/WeekView.kt
+++ app/src/main/java/com/jmrj/calendar/week/WeekView.kt
@@ -21,6 +21,8 @@ class WeekView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
         calendar
     }
 
+    private val currentTimeCalendar: Calendar by lazy { Calendar.getInstance() }
+
     private val X_PARTITION_RATIO = 1 / 7f
     private val Y_PARTITION_RATIO = 1 / 24f
 
@@ -33,10 +35,28 @@ class WeekView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
         p
     }
 
+    private val currentHourCirclePaint: Paint by lazy {
+        val p = Paint()
+        p.color = Color.BLUE
+        p.isAntiAlias = true
+        p.style = Paint.Style.FILL
+        p
+    }
+
+    private val currentHourPaint: Paint by lazy {
+        val p = Paint()
+        p.color = Color.BLUE
+        p.isAntiAlias = true
+        p.style = Paint.Style.STROKE
+        p.strokeWidth = 2f
+        p
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         this.drawHorizontalLines(canvas)
         this.drawVerticalLines(canvas)
+        this.drawCurrentHour(canvas)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -57,6 +77,18 @@ class WeekView @JvmOverloads constructor(context: Context, attrs: AttributeSet? 
             //Horizontal
             canvas.drawLine(0f, this.height * (Y_PARTITION_RATIO * i), this.width.toFloat(), this.height * (Y_PARTITION_RATIO * i), this.linesPaint)
         }
+    }
+
+    private fun drawCurrentHour(canvas: Canvas) {
+        canvas.drawLine(this.width / 7f, this.height * (Y_PARTITION_RATIO * this.getCurrentHourInDecimalFormat()), this.width.toFloat() - (this.width.toFloat() / 28f), this.height * (Y_PARTITION_RATIO * this.getCurrentHourInDecimalFormat()), this.currentHourPaint)
+        canvas.drawCircle((this.width / 7f) + 10f, this.height * (Y_PARTITION_RATIO * this.getCurrentHourInDecimalFormat()), 10f, this.currentHourCirclePaint)
+    }
+
+    private fun getCurrentHourInDecimalFormat(): Float {
+        val minutes = this.currentTimeCalendar.get(Calendar.MINUTE) / 100f
+        val hours = this.currentTimeCalendar.get(Calendar.HOUR_OF_DAY)
+        val result = (minutes * 100f) / 60f
+        return hours + result
     }
 
     fun setWeekOfTheYear(weekOfTheYear: Int) {
