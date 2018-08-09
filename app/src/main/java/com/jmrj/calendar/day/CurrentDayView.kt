1dddea5402828e21d812ae094ffb9a18f8eabc13
Jesús Manuel Ramos Juárez <jramos@ia.com.mx>
2018-08-09 09:33:00 -0500

    CurrentDayView, set day color

--- app/src/main/java/com/jmrj/calendar/day/CurrentDayView.kt
+++ app/src/main/java/com/jmrj/calendar/day/CurrentDayView.kt
@@ -8,10 +8,12 @@ import java.util.*
 
 internal class CurrentDayView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
 
+    private val locale: Locale by lazy { Locale.getDefault() }
+
     private val dayCalendar: Calendar by lazy {
-        val calendar: Calendar = Calendar.getInstance(Locale.getDefault())
-        calendar.set(Calendar.MONTH, Calendar.SEPTEMBER)
-        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        val calendar: Calendar = Calendar.getInstance(this.locale)
+        calendar.set(Calendar.DAY_OF_YEAR, 1)
+        calendar.clear(Calendar.HOUR)
         calendar.clear(Calendar.HOUR_OF_DAY)
         calendar.clear(Calendar.MINUTE)
         calendar.clear(Calendar.SECOND)
@@ -19,9 +21,53 @@ internal class CurrentDayView @JvmOverloads constructor(context: Context, attrs:
         calendar
     }
 
-    private val locale: Locale by lazy { Locale.getDefault() }
+    private val currentTimeCalendar: Calendar by lazy { Calendar.getInstance(this.locale) }
 
-    private val dayNumberPaint: Paint by lazy {
+    private val currentDay: Int by lazy { this.currentTimeCalendar.get(Calendar.DAY_OF_YEAR) }
+
+    private var selectedDayOfTheYear: Int = 1
+
+    private val previousDayNumberPaint: Paint by lazy {
+        val p = Paint()
+        p.color = Color.LTGRAY
+        p.isAntiAlias = true
+        p.style = Paint.Style.FILL
+        p.textAlign = Paint.Align.CENTER
+        p.textSize = 48f
+        p
+    }
+
+    private val previousDayNamePaint: Paint by lazy {
+        val p = Paint()
+        p.color = Color.LTGRAY
+        p.isAntiAlias = true
+        p.style = Paint.Style.FILL
+        p.textAlign = Paint.Align.CENTER
+        p.textSize = 24f
+        p
+    }
+
+    private val currentDayNumberPaint: Paint by lazy {
+        val p = Paint()
+        p.color = Color.BLUE
+        p.isAntiAlias = true
+        p.style = Paint.Style.FILL
+        p.textAlign = Paint.Align.CENTER
+        p.textSize = 48f
+        p
+    }
+
+    private val currentDayNamePaint: Paint by lazy {
+        val p = Paint()
+        p.color = Color.BLUE
+        p.isAntiAlias = true
+        p.style = Paint.Style.FILL
+        p.textAlign = Paint.Align.CENTER
+        p.textSize = 24f
+        p
+    }
+
+    private val nextDayNumberPaint: Paint by lazy {
         val p = Paint()
         p.color = Color.DKGRAY
         p.isAntiAlias = true
@@ -31,7 +77,7 @@ internal class CurrentDayView @JvmOverloads constructor(context: Context, attrs:
         p
     }
 
-    private val dayNamePaint: Paint by lazy {
+    private val nextDayNamePaint: Paint by lazy {
         val p = Paint()
         p.color = Color.DKGRAY
         p.isAntiAlias = true
@@ -63,13 +109,32 @@ internal class CurrentDayView @JvmOverloads constructor(context: Context, attrs:
     }
 
     fun setDay(dayOfTheYear: Int) {
+        this.selectedDayOfTheYear = dayOfTheYear
         this.dayCalendar.set(Calendar.DAY_OF_YEAR, dayOfTheYear)
         this.invalidate()
     }
 
     private fun drawDay(currentDayCalendar: Calendar, canvas: Canvas) {
-        val y = ((this.height / 4) + (this.dayNumberPaint.textSize / 3)) + 8
-        canvas.drawText(String.format("%02d", currentDayCalendar.get(Calendar.DAY_OF_MONTH)), this.width / 14f, y, this.dayNumberPaint)
-        canvas.drawText(currentDayCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, this.locale), this.width / 14f, (y + (this.dayNumberPaint.textSize / 3)) + 16, this.dayNamePaint)
+        val dayNumberPaint = this.getDayNumberPaint()
+        val dayNamePaint = this.getDayNamePaint()
+        val y = ((this.height / 4) + (dayNamePaint.textSize / 3)) + 8
+        canvas.drawText(String.format("%02d", currentDayCalendar.get(Calendar.DAY_OF_MONTH)), this.width / 14f, y, dayNumberPaint)
+        canvas.drawText(currentDayCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, this.locale), this.width / 14f, (y + (dayNumberPaint.textSize / 3)) + 16, dayNamePaint)
+    }
+
+    private fun getDayNumberPaint(): Paint {
+        return when {
+            this.selectedDayOfTheYear == this.currentDay -> this.currentDayNumberPaint
+            this.selectedDayOfTheYear < this.currentDay -> this.previousDayNumberPaint
+            else -> this.nextDayNumberPaint
+        }
+    }
+
+    private fun getDayNamePaint(): Paint {
+        return when {
+            this.selectedDayOfTheYear == this.currentDay -> this.currentDayNamePaint
+            this.selectedDayOfTheYear < this.currentDay -> this.previousDayNamePaint
+            else -> this.nextDayNamePaint
+        }
     }
 }
